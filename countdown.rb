9fdd4588f6987a21f463c8c49fb68c6a98b68a29
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-17 21:54:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  number = 10 
+  
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+   return "HAPPY NEW YEAR!"
 end
+
+def countdown(number)
+  number = 10 
+  
+  while number > 0 
+   sleep(5)
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+   return "HAPPY NEW YEAR!"
+end
+
